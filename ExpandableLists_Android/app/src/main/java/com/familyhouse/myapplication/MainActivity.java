6f70f5f0222029d89017f164ba6c07036e395bcc
Sammy Abdulkader <46463233+sabdul2021@users.noreply.github.com>
2020-03-15 20:49:09 -0400

    Seaman

--- ExpandableLists_Android/app/src/main/java/com/familyhouse/myapplication/MainActivity.java
+++ ExpandableLists_Android/app/src/main/java/com/familyhouse/myapplication/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupToolbar();
 
         Button button2 = findViewById(R.id.button);
         button2.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +29,15 @@ public class MainActivity extends AppCompatActivity {
     public void openActivity2() {
         Intent intent = new Intent(this, Activity2.class);
         startActivity(intent);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+
+        setSupportActionBar(toolbar);
+        mTitle.setText(toolbar.getTitle());
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
